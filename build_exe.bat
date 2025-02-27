af3f6e0d4421b1879ec4f33b5925d22999759e25
Sic <stdsic9900@naver.com>
2025-02-27 22:08:58 +0900

    Setup 생성 전 버전 리소스 추가

--- build_exe.bat
+++ build_exe.bat
@@ -1,2 +1,2 @@
 g++ -static-libgcc -static-libstdc++ -c -o main.o main.cpp -mwindows -municode
-g++ -static-libgcc -static-libstdc++ -o ColorFromPoint.exe -s main.o -mwindows -municode -L. -lMyApiDll
+g++ -static-libgcc -static-libstdc++ -o ColorFromPoint.exe -s main.o resource.o -mwindows -municode -L. -lMyApiDll
